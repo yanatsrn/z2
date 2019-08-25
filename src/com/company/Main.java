8aee9529c9ccaa4c577708d644f8e5e222b93ed5
yanatsrn <54328632+yanatsrn@users.noreply.github.com>
2019-08-26 01:06:08 +0300

    Одежда

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	double temperature=26;
+	if(temperature>25){
+        System.out.println("Наденьте шорты и в аквапарк ");
+    }
+    else {
+        if(temperature>18){
+            System.out.println("Наденьте футболку");
+        }
+        else {
+            if(temperature>10){
+                System.out.println("Наденьте свитер");
+            }
+            else {
+                System.out.println("Наденьте куртку");
+            }
+        }
+
+
+        }
+
+        }
+
+
+    }
+
